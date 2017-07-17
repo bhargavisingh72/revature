cdf082cf3bf1d68f0a36208b6a2d95e9d05ac5e5
bhargavisingh72 <bhargavisingh72@gmail.com>
2017-07-17 10:16:30 +0530

    Basic Program

---
+++ src/com/revature/Sample4.java
@@ -0,0 +1,20 @@
+package com.revature;
+
+public class Sample4 {
+	private static boolean hungry;
+	boolean teenyBit = true;
+	public void eatIfHungry(boolean hungry) {
+		 if (hungry) {
+		 int bitesOfCheese = 1;
+		 {
+		 
+		 System.out.println(bitesOfCheese);
+		 }
+		}
+		 System.out.println(teenyBit); 
+		 }
+	public static void main(String... args) {
+		Sample4 t=new Sample4();
+		t.eatIfHungry(hungry);
+	}
+}
